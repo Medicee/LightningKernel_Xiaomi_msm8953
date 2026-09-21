046b9a97ec57d94c820055dadde2bb085a7929eb
Medicee <drj9710@gmail.com>
2026-09-21 15:27:52 +0530

    Update net_invalid_timestamp to return ktime_set

--- include/linux/skbuff.h
+++ include/linux/skbuff.h
@@ -3257,7 +3257,7 @@ static inline ktime_t net_timedelta(ktime_t t)
 
 static inline ktime_t net_invalid_timestamp(void)
 {
-	return 0;
+	return ktime_set(0, 0);
 }
 
 struct sk_buff *skb_clone_sk(struct sk_buff *skb);
